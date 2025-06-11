c4c22c847476c38de573ec980baafd28f71a0a5e
klevi0212 <klevi.smula@gmail.com>
2025-06-11 17:37:57 +0200

    aggiunto seconda e terza parte

--- src/components/NewsSidebar.jsx
+++ src/components/NewsSidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NewsSidebar.css";
 import News from "./News";
+import svg from "../assets/Logo e Svg/heart-like-favorite-svgrepo-com.svg";
 const NewsSidebar = () => {
   const data = [
     {
@@ -11,8 +12,7 @@ const NewsSidebar = () => {
         "Dai un'occhiata alle novità del Nintendo eShop di questa settimana!",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/In-evidenza-nel-Nintendo-eShop-24-04-2025-2806712.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/april_31/1x1_WeeklyeShopHighlights_20250424_ITit_bannerTM_MD.jpg",
+      image: "/altre/1x1_WeeklyeShopHighlights_20250424_ITit_image950w.jpg",
     },
     {
       title: "In evidenza nel Nintendo eShop – 17/04/2025",
@@ -22,8 +22,7 @@ const NewsSidebar = () => {
         "Le novità del Nintendo eShop di questa settimana sono arrivate!",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/In-evidenza-nel-Nintendo-eShop-17-04-2025-2801434.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/april_31/1x1_WeeklyeShopHighlights_20250417_ITit_bannerTM_MD.jpg",
+      image: "/altre/1x1_WeeklyeShopHighlights_20250417_ITit_image950w.jpg",
     },
     {
       title: "In evidenza nel Nintendo eShop – 10/04/2025",
@@ -32,8 +31,7 @@ const NewsSidebar = () => {
       description: "Cosa c'è di nuovo nel Nintendo eShop? Diamo un'occhiata!",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/In-evidenza-nel-Nintendo-eShop-10-04-2025-2798050.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/april_31/1x1_WeeklyeShopHighlights_20250410_ITit_bannerTM_MD.jpg",
+      image: "/altre/1x1_WeeklyeShopHighlights_20250410_ITit_image950w.jpg",
     },
     {
       title: "Creator's Voice: The Duskbloods",
@@ -44,7 +42,7 @@ const NewsSidebar = () => {
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Creator-s-Voice-The-Duskbloods-2796823.html",
       image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/april_31/creatorsvoice/cv_td/1x1_NSwitch2_TheDuskbloods_CreatorsVoice_S2Bar_bannerTM_MD.jpg",
+        "/altre/1x1_NSwitch2_TheDuskbloods_CreatorsVoice_S2Bar_image950w.jpg",
     },
     {
       title: "Chiedi allo sviluppatore, parte 17: GameChat - Capitolo 1",
@@ -53,8 +51,7 @@ const NewsSidebar = () => {
       description: "Una funzione distintiva",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Chiedi-allo-sviluppatore-parte-17-GameChat-Capitolo-1-2788610.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/may_36/atd18/key_visuals/1x1_NSwitch2_ask_the_Dev18_IT_it_bannerTM_MD.jpg",
+      image: "/altre/1x1_AtD17_IT_it_image950w.jpg",
     },
     {
       title: "Chiedi allo sviluppatore, parte 17: GameChat - Capitolo 2",
@@ -63,8 +60,7 @@ const NewsSidebar = () => {
       description: "Atmosfera da festival",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Chiedi-allo-sviluppatore-parte-17-GameChat-Capitolo-2-2788657.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/may_36/atd18/key_visuals/1x1_NSwitch2_ask_the_Dev18_IT_it_bannerTM_MD.jpg",
+      image: "/altre/1x1_AtD17_IT_it_image950w.jpg",
     },
     {
       title: "Chiedi allo sviluppatore, parte 17: GameChat - Capitolo 3",
@@ -73,8 +69,7 @@ const NewsSidebar = () => {
       description: "Sotto una luce diversa",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Chiedi-allo-sviluppatore-parte-17-GameChat-Capitolo-3-2788658.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/may_36/atd18/key_visuals/1x1_NSwitch2_ask_the_Dev18_IT_it_bannerTM_MD.jpg",
+      image: "/altre/1x1_AtD17_IT_it_image950w.jpg",
     },
     {
       title:
@@ -85,8 +80,7 @@ const NewsSidebar = () => {
         "Dai un occhiata a quello che è in arrivo per Nintendo Switch Online con Nintendo Switch 2 !",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Quali-sono-le-novita-di-Nintendo-Switch-Online-su-Nintendo-Switch-2-2785954.html",
-      image:
-        "https://www.nintendo.com/eu/app/media/images/news_12/2025/april_31/what_s_new_with_nintendo_switch_online_on_nintendo_switch_2_/2x1_NSwitch2_NSO_logos.jpg",
+      image: "/altre/2x1_NSwitch2_NSO_logos.jpg",
     },
     {
       title:
@@ -96,8 +90,7 @@ const NewsSidebar = () => {
       description: "Valore aggiunto intrinseco",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Chiedi-allo-sviluppatore-parte-16-Nintendo-Switch-2-Capitolo-3-2787953.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/may_36/atd18/key_visuals/1x1_NSwitch2_ask_the_Dev18_IT_it_bannerTM_MD.jpg",
+      image: "/altre/1x1_AtD17_IT_it_image950w.jpg",
     },
     {
       title:
@@ -107,18 +100,89 @@ const NewsSidebar = () => {
       description: "Un nuovo standard",
       link: "",
       // "https://www.nintendo.com/it-it/Notizie/2025/Aprile/Chiedi-allo-sviluppatore-parte-16-Nintendo-Switch-2-Capitolo-4-2787954.html",
-      image:
-        "https://www.nintendo.com/eu/media/images/news_12/2025/may_36/atd18/key_visuals/1x1_NSwitch2_ask_the_Dev18_IT_it_bannerTM_MD.jpg",
+      image: "/altre/1x1_AtD17_IT_it_image950w.jpg",
+    },
+  ];
+  const secondData = [
+    {
+      title: "Cipher Monk",
+      date: "29/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+
+      image: "/altre/1x1_CipherMonk_image950w.jpg",
+    },
+    {
+      title: "Fantasy Life",
+      date: "29/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_FantasyLifeITheGirl_WhoStealsTime_ITit_image950w.jpg",
+    },
+    {
+      title: "Hannah's Day",
+      date: "29/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_HannahsDay_image950w.jpg",
+    },
+    {
+      title: "Knight's Night",
+      date: "30/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+
+      image: "/altre/1x1_KnightsNight_image950w.jpg",
+    },
+  ];
+  const thirdData = [
+    {
+      title: "Fantasy Life && Capcom Fighting Collection",
+      date: "03/05/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_NSwitch_UpcomingGames_May_IT_it_image950w.jpg",
+    },
+    {
+      title: "High On Life && Spiritfall",
+      date: "26/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_WeeklyeShopHighlights_20250508_ITit_image950w.jpg",
+    },
+    {
+      title: "American Arcadia",
+      date: "25/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_WeeklyeShopHighlights_20250515_IT_it_image950w.jpg",
+    },
+    {
+      title: "Pancho's Missions",
+      date: "25/04/2025",
+      platform: "Nintendo Switch",
+      svg: svg,
+      link: "",
+      image: "/altre/1x1_PanchosMission_image950w.jpg",
     },
   ];
   return (
     <>
       <aside className="news-sidebar">
         <div>
-          <h2 className="ultime-novita">
-            <News title="Ultime novità" />
-          </h2>
           <ul className="news-list">
+            <li>
+              <h2 className="ultime-novita">
+                <News title="Ultime novità" />
+              </h2>
+            </li>
             {data.map((item, i) => (
               <li key={i} className="news-item">
                 <a href={item.link}>
@@ -126,7 +190,7 @@ const NewsSidebar = () => {
                   <div className="news-info">
                     <h3 className="news-title">{item.title}</h3>
                     <span className="news-date">
-                      {item.date} - {item.platform}
+                      {item.date} | {item.platform}
                     </span>
                     <div className="news-description">{item.description}</div>
                   </div>
@@ -134,15 +198,71 @@ const NewsSidebar = () => {
               </li>
             ))}
             <li>
-              <h3 className="piu-notizie"> PIU' NOTIZIE{">>"}</h3>
+              <a href="" className="side-writting">
+                {" "}
+                PIU' NOTIZIE{">>"}
+              </a>
             </li>
           </ul>
         </div>
         <div>
-          <News title="Prossimi giochi" />
+          <ul className="news-list">
+            <li>
+              <h2 className="prossimi-giochi">
+                <News title="Prossimi giochi" />
+              </h2>
+            </li>
+            {secondData.map((item, i) => (
+              <li key={i} className="news-item">
+                <a href={item.link}>
+                  <img src={item.image} />
+                  <div className="news-info">
+                    <h3 className="news-title">{item.title}</h3>
+                    <span className="news-date">
+                      {item.date} · {item.platform}
+                    </span>
+                    {/* <div className="news-svg">{item.svg}</div> */}
+                  </div>
+                </a>
+                <a className="news-svg">{item.svg}</a>
+              </li>
+            ))}
+            <li>
+              <a href="" className="side-writting">
+                VEDI TUTTI I GIOCHI{">>"}
+              </a>
+            </li>
+          </ul>
         </div>
         <div>
-          <News title="Uscito di recente" />
+          <ul className="news-list">
+            <li>
+              <h2 className="uscito-di-recente">
+                <News title="Uscito di recente" />
+              </h2>
+            </li>
+            {thirdData.map((item, i) => (
+              <li key={i} className="news-item">
+                <a href={item.link}>
+                  <img src={item.image} />
+                  <div className="news-info">
+                    <h3 className="news-title">{item.title}</h3>
+                    <span className="news-date">
+                      {item.date} · {item.platform}
+                    </span>
+                    {/* <div className="news-svg">{item.svg}</div> */}
+                  </div>
+                </a>
+                <a className="news-svg">{item.svg}</a>
+              </li>
+            ))}
+            <li>
+              <a href="" className="side-writting">
+                {" "}
+                VEDI TUTTI I GIOCHI {">>"}
+              </a>
+            </li>
+          </ul>
         </div>
       </aside>
     </>
