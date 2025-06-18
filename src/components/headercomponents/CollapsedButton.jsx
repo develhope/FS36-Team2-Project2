7706e8032edddbc061cd73be860bc0bccfbfd6f9
Giuseppe Vigneri <geppe1990@gmail.com>
2025-06-18 14:58:05 +0200

    Fix after merge

--- src/components/headercomponents/CollapsedButton.jsx
+++ src/components/headercomponents/CollapsedButton.jsx
@@ -10,26 +10,28 @@ export function CollapsedButton(){
     return(
          <>
         <div className="collapsed">
-        <img className="nintendo-logo-footer" src="/nintendo-logo.svg" />
-            <button className="collapsed-button" onClick={handleToggleCollapse}>
-                <img src="/globe.svg" />
-                Italia
+            <div className={"collapsed-top"}>
+                <img className="nintendo-logo-footer" src="/nintendo-logo.svg"/>
+                <button className="collapsed-button" onClick={handleToggleCollapse}>
+                    <img src="/globe.svg"/>
+                    Italia
                     <div className="arrow-globe">
-                        <img src="/arrow-globe1.svg" />
-                        <img src="/arrow-globe2.svg" />
+                        <img src="/arrow-globe1.svg"/>
+                        <img src="/arrow-globe2.svg"/>
                     </div>
                 </button>
-            {collapsed && 
-            <div className="collapsedBox">
-                <ul>
-                    <li><a href="">België</a></li>
-                    <li><a href="">Belgique</a></li>
-                    <li><a href="">Deutschland</a></li>
-                    <li><a href="">España</a></li>
-                    <li><a href="">France</a></li>
-                </ul>
-                <ul>
-                    <li><a href="">Italia</a></li>
+            </div>
+            {collapsed &&
+                <div className="collapsedBox">
+                    <ul>
+                        <li><a href="">België</a></li>
+                        <li><a href="">Belgique</a></li>
+                        <li><a href="">Deutschland</a></li>
+                        <li><a href="">España</a></li>
+                        <li><a href="">France</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="">Italia</a></li>
                     <li><a href="">Nederland</a></li>
                     <li><a href="">Österreich</a></li>
                     <li><a href="">Portugal</a></li>
@@ -45,7 +47,8 @@ export function CollapsedButton(){
                         {fontWeight : "bold"}
                     }><a href="">Altre Regioni</a></li>
                 </ul>
-            </div>}
+            </div>
+            }
         </div>
         </>
     )
